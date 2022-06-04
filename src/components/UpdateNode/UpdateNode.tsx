4e792752d8243f7b1cf166bef311accd38a57704
Lucho Andrade <luan95_12@hotmail.com>
2022-06-04 15:32:51 -0500

    Fix nodes crossing

--- src/components/UpdateNode/UpdateNode.tsx
+++ src/components/UpdateNode/UpdateNode.tsx
@@ -1,8 +1,8 @@
-import { useCallback, useMemo } from "react";
+import { useCallback, useMemo, useRef } from "react";
 import ReactFlow, { useNodesState, useEdgesState } from "react-flow-renderer";
 import { ReactFlowInstance } from "react-flow-renderer";
 import { addEdge, Connection } from "react-flow-renderer";
-import { Node } from "react-flow-renderer";
+import { Node, OnNodesChange } from "react-flow-renderer";
 import { createGraphLayout } from "../../utils";
 import CustomNode from "../CustomNode/CustomNode";
 
@@ -10,6 +10,8 @@ const UpdateNode = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState<Node[]>([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const nodeTypes = useMemo(() => ({ customNode: CustomNode }), []);
+  const fitViewRef = useRef(() => {});
+  const nodesLengthRef = useRef(edges.length);
 
   const onConnect = useCallback(
     (params: Connection) => {
@@ -18,57 +20,77 @@ const UpdateNode = () => {
     [setEdges]
   );
 
-  const onInit = (reactFlowInstance: ReactFlowInstance) => {
-    createGraphLayout(
-      reactFlowInstance.getNodes(),
-      reactFlowInstance.getEdges()
-    )
-      .then((els) => setNodes(els))
-      .catch((err) => console.error(err));
-    reactFlowInstance.fitView();
+  const onInit = async (reactFlowInstance: ReactFlowInstance) => {
+    try {
+      const nodes = await createGraphLayout(
+        reactFlowInstance.getNodes(),
+        reactFlowInstance.getEdges()
+      );
+      setNodes(nodes);
+      reactFlowInstance.fitView();
+      fitViewRef.current = reactFlowInstance.fitView;
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const createNode = (parentNode: Node | null, id: string): Node => {
     const newNode = {
       id,
       type: "customNode",
-      data: {
-        label: `Node ${id}`,
-      },
+      data: { label: `Node ${id}` },
       position: { x: 0, y: 0 },
     };
-    if (parentNode) {
+    const { width, height } = parentNode ?? {};
+    if (parentNode && width && height) {
       const { position } = parentNode;
       newNode.position = {
-        // @ts-ignore
-        x: position.x - parentNode.width / 2 + Math.random() / 1000,
-        // @ts-ignore
-        y: position.y - parentNode.height / 2,
+        x: position.x,
+        y: position.y + height + 25,
       };
     }
     return newNode;
   };
 
-  const nodeClickHandler = (node: Node | null) => {
-    setNodes((prev) => {
-      const newNode = createNode(node, `${prev.length}`);
-      if (node) {
-        setEdges((prev) =>
-          addEdge(
-            {
-              id: `${node.id}-${newNode.id}`,
-              source: node.id,
-              target: newNode.id,
-              sourceHandle: "a",
-              targetHandle: "b",
-            },
-            prev
-          )
-        );
+  const nodeClickHandler = useCallback(
+    (node: Node | null) => {
+      setNodes((prev) => {
+        const newNode = createNode(node, `${prev.length}`);
+        if (node) {
+          setEdges((prev) =>
+            addEdge(
+              {
+                id: `${node.id}-${newNode.id}`,
+                source: node.id,
+                target: newNode.id,
+                sourceHandle: "a",
+                targetHandle: "b",
+              },
+              prev
+            )
+          );
+        }
+        return [...prev, newNode];
+      });
+    },
+    [setEdges, setNodes]
+  );
+
+  const nodesChangeHandler: OnNodesChange = useCallback(
+    async (nodeChanges) => {
+      onNodesChange(nodeChanges);
+      if (nodesLengthRef.current !== nodes.length) {
+        const newNodes = await createGraphLayout(nodes, edges);
+        setNodes(newNodes);
+        nodesLengthRef.current = nodes.length;
       }
-      return [...prev, newNode];
-    });
-  };
+      // TODO: mejorar
+      setTimeout(() => {
+        fitViewRef.current();
+      }, 500);
+    },
+    [edges, nodes, setNodes, onNodesChange]
+  );
 
   return (
     <>
@@ -76,7 +98,7 @@ const UpdateNode = () => {
       <ReactFlow
         nodes={nodes}
         edges={edges}
-        onNodesChange={onNodesChange}
+        onNodesChange={nodesChangeHandler}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         onInit={onInit}
@@ -85,7 +107,7 @@ const UpdateNode = () => {
         snapToGrid={true}
         defaultZoom={1.5}
         fitView
-        attributionPosition="bottom-left"
+        // attributionPosition="bottom-left"
       ></ReactFlow>
     </>
   );
--- src/utils.ts
+++ src/utils.ts
@@ -7,18 +7,34 @@ Get a sense of the parameters at:
 https://rtsys.informatik.uni-kiel.de/elklive/examples.html?e=general%2Fspacing%2FnodesEdges 
 */
 
-const DEFAULT_WIDTH = 330;
-const DEFAULT_HEIGHT = 75;
-const DEFAULT_WIDTH_FOR_ROOT = 170;
+const DEFAULT_WIDTH = 50;
+const DEFAULT_HEIGHT = 30;
+const DEFAULT_WIDTH_FOR_ROOT = 50;
 
 const elk = new Elk({
   defaultLayoutOptions: {
     "elk.algorithm": "layered",
-    "elk.direction": "RIGHT",
-    "elk.spacing.nodeNode": "75",
-    "elk.layered.spacing.nodeNodeBetweenLayers": "75",
+    "elk.direction": "DOWN",
+    "elk.spacing.nodeNode": "100",
+    "elk.layered.spacing.nodeNodeBetweenLayers": "100",
+    "elk.layered.crossingMinimization.strategy": "INTERACTIVE",
   },
 });
+
+export const calculateLayout = (flowNode: Node, graph: ElkNode) => {
+  const node = graph?.children?.find((n) => n.id === flowNode.id);
+  if (!node) return flowNode;
+  const { x, y, width, height } = node;
+  if (x && y && width && height) {
+    flowNode.position = {
+      x: x - width,
+      y: y - height,
+    };
+  } else {
+    console.log("error? ");
+  }
+  return flowNode;
+};
 
 export const createGraphLayout = async (
   nodes: Array<Node>,
@@ -52,14 +68,5 @@ export const createGraphLayout = async (
     edges: elkEdges,
   });
 
-  return nodes.map((flowNode) => {
-    const node = newGraph?.children?.find((n) => n.id === flowNode.id);
-    if (node?.x && node?.y && node?.width && node?.height) {
-      flowNode.position = {
-        x: node.x - node.width / 2 + Math.random() / 1000,
-        y: node.y - node.height / 2,
-      };
-    }
-    return flowNode;
-  });
+  return nodes.map((flowNode) => calculateLayout(flowNode, newGraph));
 };
