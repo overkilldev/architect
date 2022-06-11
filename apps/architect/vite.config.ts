9d083385a2b59fa4c0ad8bb65b1bb85146affb03
Lucho Andrade <luan95_12@hotmail.com>
2022-06-11 09:48:37 -0500

    feat(global): add vite and vitest

---
+++ apps/architect/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest/config" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  build: {
+    outDir: "build"
+  },
+  plugins: [tsconfigPaths(), svgrPlugin(), react()],
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser"
+    }
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.tsx"
+  }
+});
