e255371518afd3f2738185b1845bc1f59622c3b8
Lucho Andrade <luan95_12@hotmail.com>
2022-06-12 18:25:51 -0500

    feat(architect): pass node data to the drawer

--- apps/architect/src/components/NodeDrawer/NodeDrawer.tsx
+++ apps/architect/src/components/NodeDrawer/NodeDrawer.tsx
@@ -1,18 +1,26 @@
-import React, { FormEvent } from "react";
+import React, { ChangeEvent, memo, useCallback, useState } from "react";
+import { FormEvent } from "react";
 
 import { NodeDrawerProps as Props } from "./NodeDrawer.types";
 import Drawer from "components/global/Drawer/Drawer";
+import Input from "components/global/Input/Input";
 
 const NodeDrawer: React.FC<Props> = props => {
   const { selectedNode, onClose, ...rest } = props;
-  const { label, setNodes } = selectedNode.data;
+  const { id, data } = selectedNode;
+  const { label, setNodes } = data;
+  const [formLabel, setFormLabel] = useState(label);
+
+  const changeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setFormLabel(e.target.value);
+  }, []);
 
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setNodes(prev => {
       return prev.map(node => {
         if (node.id === selectedNode.id) {
-          return { ...node, data: { ...node.data, label: "soy lo máximo" } };
+          return { ...node, data: { ...node.data, label: formLabel } };
         }
         return node;
       });
@@ -29,9 +37,23 @@ const NodeDrawer: React.FC<Props> = props => {
       {...rest}
     >
       <form onSubmit={submitHandler}>
-        <input placeholder="Type here..." />
-        <p>{label}</p>
-        <button type="submit">Save</button>
+        <p className="pb-2 text-md font-medium">ID: {id}</p>
+        <Input
+          placeholder="Node label"
+          label="Label"
+          value={formLabel}
+          onChange={changeHandler}
+        />
+        <Input
+          placeholder="Choose or add a new template."
+          label="Enhanced template"
+        />
+        <button
+          type="submit"
+          className="px-4 py-2 border rounded-lg bg-blue-500 text-white"
+        >
+          Save
+        </button>
       </form>
     </Drawer>
   );
@@ -39,4 +61,4 @@ const NodeDrawer: React.FC<Props> = props => {
 
 NodeDrawer.defaultProps = {};
 
-export default NodeDrawer;
+export default memo(NodeDrawer);
