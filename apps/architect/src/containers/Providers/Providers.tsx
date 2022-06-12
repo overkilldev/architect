a65a781e7d28cdfa4e3b2ae6e73a899e89fa2c00
Lucho Andrade <luan95_12@hotmail.com>
2022-06-12 10:18:53 -0500

    fix(architect): fix providers default height

--- apps/architect/src/containers/Providers/Providers.tsx
+++ apps/architect/src/containers/Providers/Providers.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ProvidersProps as Props } from "./Providers.types";
 
 const Providers: React.FC<Props> = props => {
-  return <div>{props.children}</div>;
+  return <>{props.children}</>;
 };
 
 Providers.defaultProps = {};
