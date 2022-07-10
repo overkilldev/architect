f665a13b8e606fb67e2a396e1888e037557866b1
Alexander Tigselema <--global>
2022-07-09 20:01:07 -0500

    chore(accounts): fix graphql types

--- services/accounts/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ services/accounts/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "accounts68933643": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "accounts": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ services/accounts/src/graphql/queries.ts
@@ -0,0 +1,340 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getAccount = /* GraphQL */ `
+  query GetAccount($id: ID!) {
+    getAccount(id: $id) {
+      id
+      name
+      lastName
+      email
+      enhancers {
+        items {
+          id
+          name
+          snippets
+          createdAt
+          updatedAt
+          accountEnhancersId
+        }
+        nextToken
+      }
+      templates {
+        items {
+          id
+          name
+          content
+          createdAt
+          updatedAt
+          accountTemplatesId
+        }
+        nextToken
+      }
+      enhancedTemplates {
+        items {
+          id
+          name
+          content
+          enhancersIds
+          templateId
+          createdAt
+          updatedAt
+          accountEnhancedTemplatesId
+        }
+        nextToken
+      }
+      trees {
+        items {
+          id
+          name
+          nodes
+          edges
+          createdAt
+          updatedAt
+          accountTreesId
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listAccounts = /* GraphQL */ `
+  query ListAccounts(
+    $filter: ModelAccountFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listAccounts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        lastName
+        email
+        enhancers {
+          nextToken
+        }
+        templates {
+          nextToken
+        }
+        enhancedTemplates {
+          nextToken
+        }
+        trees {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getEnhancer = /* GraphQL */ `
+  query GetEnhancer($id: ID!) {
+    getEnhancer(id: $id) {
+      id
+      name
+      snippets
+      account {
+        id
+        name
+        lastName
+        email
+        enhancers {
+          nextToken
+        }
+        templates {
+          nextToken
+        }
+        enhancedTemplates {
+          nextToken
+        }
+        trees {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+      accountEnhancersId
+    }
+  }
+`;
+export const listEnhancers = /* GraphQL */ `
+  query ListEnhancers(
+    $filter: ModelEnhancerFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listEnhancers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        snippets
+        account {
+          id
+          name
+          lastName
+          email
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+        accountEnhancersId
+      }
+      nextToken
+    }
+  }
+`;
+export const getTemplate = /* GraphQL */ `
+  query GetTemplate($id: ID!) {
+    getTemplate(id: $id) {
+      id
+      name
+      content
+      account {
+        id
+        name
+        lastName
+        email
+        enhancers {
+          nextToken
+        }
+        templates {
+          nextToken
+        }
+        enhancedTemplates {
+          nextToken
+        }
+        trees {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+      accountTemplatesId
+    }
+  }
+`;
+export const listTemplates = /* GraphQL */ `
+  query ListTemplates(
+    $filter: ModelTemplateFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTemplates(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        content
+        account {
+          id
+          name
+          lastName
+          email
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+        accountTemplatesId
+      }
+      nextToken
+    }
+  }
+`;
+export const getEnhancedTemplate = /* GraphQL */ `
+  query GetEnhancedTemplate($id: ID!) {
+    getEnhancedTemplate(id: $id) {
+      id
+      name
+      content
+      enhancersIds
+      templateId
+      account {
+        id
+        name
+        lastName
+        email
+        enhancers {
+          nextToken
+        }
+        templates {
+          nextToken
+        }
+        enhancedTemplates {
+          nextToken
+        }
+        trees {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+      accountEnhancedTemplatesId
+    }
+  }
+`;
+export const listEnhancedTemplates = /* GraphQL */ `
+  query ListEnhancedTemplates(
+    $filter: ModelEnhancedTemplateFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listEnhancedTemplates(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        name
+        content
+        enhancersIds
+        templateId
+        account {
+          id
+          name
+          lastName
+          email
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+        accountEnhancedTemplatesId
+      }
+      nextToken
+    }
+  }
+`;
+export const getTree = /* GraphQL */ `
+  query GetTree($id: ID!) {
+    getTree(id: $id) {
+      id
+      name
+      nodes
+      edges
+      account {
+        id
+        name
+        lastName
+        email
+        enhancers {
+          nextToken
+        }
+        templates {
+          nextToken
+        }
+        enhancedTemplates {
+          nextToken
+        }
+        trees {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+      accountTreesId
+    }
+  }
+`;
+export const listTrees = /* GraphQL */ `
+  query ListTrees(
+    $filter: ModelTreeFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTrees(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        nodes
+        edges
+        account {
+          id
+          name
+          lastName
+          email
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+        accountTreesId
+      }
+      nextToken
+    }
+  }
+`;
